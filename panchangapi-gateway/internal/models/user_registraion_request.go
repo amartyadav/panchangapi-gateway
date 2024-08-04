57b326fab31f1b4de891d8acde7597bf321b0a4f
amartya <arvyagami@proton.me>
2024-08-04 17:25:57 +0530

    remove password from UserVerificationEmailRequestStruct to split the process into two parts.

--- panchangapi-gateway/internal/models/user_registraion_request.go
+++ panchangapi-gateway/internal/models/user_registraion_request.go
@@ -1,7 +1,5 @@
 package models
 
-type UserRegistrationRequest struct {
-	Username string `json:"username" binding:"required"`
+type UserEmailVerificationRequest struct {
 	Email string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required, min=8"`
 }
