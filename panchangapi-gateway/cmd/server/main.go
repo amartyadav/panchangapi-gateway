6e07f4e40ea2c8f0da1831a4fa1a98c94ad337d3
amartya <arvyagami@proton.me>
2024-08-04 17:25:13 +0530

    change to echo framework. add / call, add /sendverificationemail route

--- panchangapi-gateway/cmd/server/main.go
+++ panchangapi-gateway/cmd/server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"panchangapi-gateway/internal/api/handlers"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    fmt.Println("PanchangAPI Gateway")
+	fmt.Println("PanchangAPI Gateway")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "PanchangAPI Gateway")
+	})
+
+	e.GET("/sendverificationemail", handlers.VerifyEmail)
+	e.Logger.Fatal(e.Start("localhost:1323"))
 }
