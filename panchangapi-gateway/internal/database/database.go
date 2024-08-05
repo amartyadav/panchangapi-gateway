bf7699c22408d9c0cac8ab06ab2e178cd24c6b8c
amartya <arvyagami@proton.me>
2024-08-05 16:43:00 +0530

    adding db ping

--- panchangapi-gateway/internal/database/database.go
+++ panchangapi-gateway/internal/database/database.go
@@ -2,18 +2,26 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
-func InitDB(databaseString string) error {
+func InitDB(databaseString string) {
 	var err error
 
 	db, err := sql.Open("postgres", databaseString)
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
 
-	return db.Ping()
+	err = db.Ping()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("[SUCCESS] DB Connected")
 }
