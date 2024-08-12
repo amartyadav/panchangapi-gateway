d2c0ead1aa932a18303fae3a8faa534903dc47fa
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2024-08-12 23:39:16 +0545

    installed dependencies and setup husky

---
+++ frontend/src/app/landingpage/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Page() {
+  return <h1>Welcome to Panchang API GateWay!</h1>;
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>;
+}
