500a70f6ebb86dd8e52e273981e47ebe4063ea4f
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2024-08-13 00:56:29 +0545

    added flowbite library

--- frontend/src/app/landingpage/page.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Page() {
-  return <h1>Welcome to Panchang API GateWay!</h1>;
-}
---
+++ frontend/src/app/test/landingpage/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PageSidebar from "../../components/sidebar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="container grid grid-cols-9">
+      <div className="sidebar col-span-1">
+        <PageSidebar />
+      </div>
+      <div className="content col-span-8">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
